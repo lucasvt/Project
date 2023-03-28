23c937eebe4ce35299e323395853fb16bc1e6744
Lucas Vaz Tassoni <lucasvt2@gmail.com>
2023-03-28 20:10:21 -0300

    customizando o status code

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { Body, Param, Post } from '@nestjs/common/decorators';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { Body, HttpCode, Param, Post, Res } from '@nestjs/common/decorators';
+import { response } from 'express';
 
 @Controller('courses')
 export class CoursesController {
     @Get()
-    findAll() {
-        return 'Listagen de Cursos';
+    findAll(@Res() response) {
+        return response.status(200).send('Listagen de Cursos');
     }
     @Get(':id')
     findOne(@Param() params ) {
@@ -13,6 +14,7 @@ export class CoursesController {
     }
 
     @Post()
+    @HttpCode(HttpStatus.NO_CONTENT)
     create(@Body('name') body) {
         return body;
 
