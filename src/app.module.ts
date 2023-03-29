1377cedab3c36e106e012df40c7014dfc32b3f8a
Lucas Vaz Tassoni <lucasvt2@gmail.com>
2023-03-29 17:47:06 -0300

    Criado modulo de cursos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesController } from './courses/courses.controller';
 import { CourseService } from './courses/courses.service';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
+  imports: [CoursesModule],
   controllers: [AppController, CoursesController],
   providers: [AppService, CourseService],
 })
